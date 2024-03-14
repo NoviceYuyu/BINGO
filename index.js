7ee3e8335779df0d156b065340fdcd63daa2dfda
Yuta Sato <yuta.abergas.sato@gmail.com>
2024-03-14 14:28:21 +0800

    Second commit, made some JS code

---
+++ index.js
@@ -0,0 +1,35 @@
+console.log("Hello World!");
+
+generateNumber (15);
+
+function generateNumber (max)
+{
+    let randomNumber = Math.floor(Math.random() * max) + 1;
+
+    if(randomNumber <= 15)
+    {
+        console.log('The number is in B.')
+    }
+    else if (randomNumber >= 16 && randomNumber <= 30)
+    {
+        console.log('The number is in I');
+    }
+    else if (randomNumber >=31 && randomNumber <=45)
+    {
+        console.log('The number is in N');
+    }
+    else if (randomNumber >=46 && randomNumber <=60)
+    {
+        console.log('The number is in G');
+    }
+    else if (randomNumber >=61 && randomNumber <=75)
+    {
+        console.log('The number is in O');
+    }
+    else
+    {
+        console.log('The number is invalid');
+    }
+
+    return console.log(randomNumber);
+}
